02f46d589444b51faf7de70b3e425be6353dcc79
Show0ff <khlopinMV@gmail.com>
2022-07-06 12:16:17 +0300

    Сделал считывание с одну строчку через Files

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,30 +1,27 @@
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Runner {
 
     public static void main(String[] args) {
 
+        try {
+            List<String> textString = Files.readAllLines(Path.of("C:\\JAVA\\CryptoAnaliser\\CryptoAnaliser\\src\\other\\crypt.txt"));
 
-        try (FileChannel channel = FileChannel.open(Path.of("C:\\JAVA\\CryptoAnaliser\\CryptoAnaliser\\src\\other\\crypt.txt"))){
-            ByteBuffer buffer = ByteBuffer.allocate((int) channel.size());
+            System.out.println(textString);
 
-            channel.read(buffer);
-            List<Byte> byteBufferList = new ArrayList<>();
-            buffer.flip();
-
-            while (buffer.hasRemaining()) {
-                byteBufferList.add(buffer.get());
-            }
-            System.out.println(byteBufferList);
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
 
     }
 }
