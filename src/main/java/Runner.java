6674b01f414fb8fce2b518254e61533e7bd0d797
Show0ff <khlopinMV@gmail.com>
2022-07-07 01:42:01 +0300

    Работает смещение в право, но в лево ещё не реализовано

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -2,41 +2,42 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Runner {
 
-    private static final Path cryptText = Path.of("C:\\JAVA\\CryptoAnaliser\\CryptoAnaliser\\src\\other\\crypt.txt");
-    private static final Path originalText = Path.of("C:\\JAVA\\CryptoAnaliser\\CryptoAnaliser\\src\\other\\original.txt");
-    public static final char[] ALPHABET_RU = {'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з',
-            'и', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ',
-            'ъ', 'ы', 'ь', 'э', 'я', '.', ',', '«', '»', '"', '\'', ':', '!', '?', ' ', '-'};
+    private static final Path cryptText = Path.of("C:\\JAVA\\MyProjects\\MyCryptoAnalyser\\src\\other\\cryptText.txt");
+    private static final Path originalText = Path.of("C:\\JAVA\\MyProjects\\MyCryptoAnalyser\\src\\other\\originalText.txt");
+    private static final List<Character> ALPHABET = Arrays.asList('а', 'б', 'в',
+            'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у',
+            'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'я', '.', ',', '«', '»',
+            ':', '!', '?', ' ');
 
     public static void main(String[] args) {
-
-        getTextForCrypt();
-        getKey();
+        String result = encrypting(getTextForCrypt(), setKey(), getWay());
+        System.out.println(result);
     }
 
-    private static List<String> getTextForCrypt() {
-        List<String> textString = new ArrayList<>();
+    private static String getTextForCrypt() { // получить текст для шифровки
+        String textString = null;
         try {
             if (Files.exists(originalText)) {
-                textString = Files.readAllLines(originalText);
-                return textString;
+                textString = String.valueOf(Files.readAllLines(originalText));
+                return textString.substring(1,textString.length() - 1);
             } else {
-                System.out.print("Файла не существует");
+                System.out.println("Файла не существует");
             }
-
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    return textString;}
+        return textString;
+    }
 
-    private static int getKey() {
+    private static int setKey() { // установить значение смещения
         Scanner console = new Scanner(System.in);
-        System.out.println("Введите ключ: ");
+        System.out.println("Введите число на которое хотите сделать смещение: ");
         while (true) {
             int key = console.nextInt();
             if (key <= 32 && key > 0) {
@@ -44,23 +45,34 @@ public class Runner {
             } else {
                 System.out.println("Введите ключ в пределах длинны алфавита: ");
                 key = console.nextInt();
-
             }
         }
     }
 
-    private static String encrypting() {
-        List<String> textForCrypt = getTextForCrypt();
-        System.out.println();
+    private static String getWay() { //метод получения направления смещения
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите сторону смещения L или R");
+        while (true) {
+            String result = scanner.nextLine();
+            if (result.equalsIgnoreCase("r")) {
+                return "R";
+            } else if (result.equalsIgnoreCase("l")) {
+                return "L";
+            } else {
+                System.out.println("Неверное направление");
+            }
+        }
+    }
 
 
-    return null;}
-
-
-
-    private static String printCryptResult() {
-
-        return null;
-    };
-
+    private static String encrypting(String text, int key, String way) { //метод шифрования
+        // сделать проверку на пустоту
+        char[] charArray = text.toCharArray();
+        for (int i = 0; i < text.length(); i++) {
+            int temp = ALPHABET.indexOf(text.charAt(i)); // Получаем индекс символа в алфавите из входящего текста
+            charArray[i] = ALPHABET.get(temp + key);
+        }
+        return String.valueOf(charArray);
+    }
 }
+//[АБВГДЕЁЖЗИЙКЛМН]
