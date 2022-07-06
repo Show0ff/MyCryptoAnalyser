9c8869b289cee346931e810fa1d6ee3caf4c79d4
Show0ff <khlopinMV@gmail.com>
2022-07-07 02:59:56 +0300

    Разбил на классы#2

--- src/main/java/Encrypting.java
+++ src/main/java/Encrypting.java
@@ -5,10 +5,13 @@ public class Encrypting {
         char[] charArray = text.toCharArray();
         for (int i = 0; i < text.length(); i++) {
             int temp = Constants.getALPHABET().indexOf(text.charAt(i)); // Получаем индекс символа в алфавите из входящего текста
-            if (way.equalsIgnoreCase("R")) {
-                charArray[i] = Constants.getALPHABET().get(temp + key);
-            } else if (way.equalsIgnoreCase("L")) {
-                charArray[i] = Constants.getALPHABET().get(temp - key); // бросает ошибку index - 1
+            if (temp == -1) {
+                i++;
+                if (way.equalsIgnoreCase("R")) {
+                    charArray[i] = Constants.getALPHABET().get(temp + key);
+                } else if (way.equalsIgnoreCase("L")) {
+                    charArray[i] = Constants.getALPHABET().get(temp - key); // бросает ошибку index - 1
+                }
             }
         }
         return String.valueOf(charArray);
